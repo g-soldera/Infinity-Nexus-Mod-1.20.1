14aa1e47bddf27a677234e0d4bcd52f984a0fc1f
Reaviik <reavik_gamer@hotmail.com>
2024-09-23 15:31:45 -0300

    3.1.0-1.20.1 - Fix Placer
    Signed-off-by: Reaviik <reavik_gamer@hotmail.com>

--- src/main/java/com/Infinity/Nexus/Mod/block/entity/wrappedHandlerMap/SqueezerHandler.java
+++ src/main/java/com/Infinity/Nexus/Mod/block/entity/wrappedHandlerMap/SqueezerHandler.java
@@ -10,14 +10,14 @@ public class SqueezerHandler {
 
     public static boolean extract(int slot, Direction direction) {
         return switch (slot) {
-            case 3 -> true; //Output
+            case 1,3 -> true; //Output
             case 4,5,6,7,8 -> direction == Direction.UP; //Upgrade and Component
             default -> false;
         };
     }
     public static boolean insert(int slot, @NotNull ItemStack stack) {
         return switch (slot) {
-            case 0,1 -> !ModUtils.isUpgrade(stack) && !ModUtils.isComponent(stack);
+            case 0 -> !ModUtils.isUpgrade(stack) && !ModUtils.isComponent(stack);
             case 2 -> stack.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).isPresent();
             case 4,5,6,7 -> ModUtils.isUpgrade(stack);
             case 8 -> ModUtils.isComponent(stack);
